9b7fec1064badd3c31db6387d36d7aa73711714d
Ariel Figueroa <arielblue721@gmail.com>
2025-02-18 20:09:57 -0500

    fix reployment error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import Layout from "../components/Layout";
 import FishlyLogo from "../components/FishlyLogo";
 
